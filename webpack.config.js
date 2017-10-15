8e60c6d2d07e930f7305e3d6cd99c8f618563a4e
gochiUSa <sinc@gpchiUSa.local>
2017-10-15 23:06:59 +0900

    build env for react building

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const fs = require('fs');
+
+module.exports = {
+  entry: {
+    index: __dirname + '/src/js/index.js',
+  },
+  output: {
+    path: __dirname + '/dist/js',
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: ['/node_modules/'],
+        query: {
+          presets: [
+            "es2015",
+            "react"
+          ]
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      files: [
+        'dist/**/**/*.js'
+      ],
+      server: {
+        baseDir: 'dist',
+        index: "index.html"
+      }
+    })
+  ]
+}
